05d5a96dbac6d62a6bcc47f47231baf76672bf43
Squidez <44018884+Squidez@users.noreply.github.com>
2021-05-12 12:10:40 +0200

    changed image checkbox

--- widgets/CollectionGridImage.py
+++ widgets/CollectionGridImage.py
@@ -2,6 +2,7 @@ from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.behaviors import ButtonBehavior
+# from kivy.core.window import Window
 from kivy.uix.image import Image
 import kivy.properties as kyprops
 
@@ -27,10 +28,18 @@ class CollectionGridImage(AnchorLayout, ButtonBehavior, Image):
     Builder.load_file('templates/CollectionGridImage.kv')
 
     source = kyprops.StringProperty("")
+    # is_hovered = False
     # the default collection prevents getting errors and warnings before
     # the widget is fully initialized, but then, it does throw an other
     # error down the line (which is caught so everything is fine)
     collection_image = kyprops.ObjectProperty(CollectionImage("shadow32.png"))
+
+    # def __init__(self, **kwargs):
+    #     super(CollectionGridImage, self).__init__(**kwargs)
+    #     # bind mouse position updates so we can check when the cursor
+    #     # hovers over the image
+    #     # note: had to do it in the init, otherwise it doesn't work
+    #     Window.bind(mouse_pos=self.on_mouse_pos)
 
     def on_press(self):
         """ Summary
@@ -48,3 +57,21 @@ class CollectionGridImage(AnchorLayout, ButtonBehavior, Image):
             app.TOOLBAR.selected_images.append(self.collection_image)
         else:
             app.TOOLBAR.selected_images.remove(self.collection_image)
+    
+    
+    # def on_mouse_pos(self, window, pos):
+    #     # TODO: figure out how to make it work also on the first element
+    #     # displayed... weird
+
+    #     empty = 'resources/empty.png'
+    #     blank_checkbox = 'resources/blank-check-box.png'
+
+    #     # if the mouse position is over the image
+    #     if self.collide_point(*pos):
+    #         if not self.is_hovered:
+    #             self.is_hovered = True
+    #             self.ids.select_image.background_checkbox_normal = empty
+    #     else:
+    #         if self.is_hovered:
+    #             self.is_hovered = False
+    #             self.ids.select_image.background_checkbox_normal = blank_checkbox
