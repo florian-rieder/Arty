6cd043815f398d909073b8bac3f6b1bc5772ec41
Sergenti <florian.rieder@unil.ch>
2022-04-05 11:45:48 +0200

    Added back btn to About screen

--- screens/AboutScreen.py
+++ screens/AboutScreen.py
@@ -1,6 +1,15 @@
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.app import App
 
 
 class AboutScreen(Screen):
     Builder.load_file('templates/AboutScreen.kv')
+
+    def on_back_released(self):
+        """
+            Callback that runs when the user clicks the "Back" button.
+        """
+        app = App.get_running_app()
+        coll_screen = app.SCREENS["START"]
+        app.SCREEN_MANAGER.switch_to(coll_screen, direction="right")
